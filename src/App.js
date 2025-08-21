30a56a6ec546bf3da9caafa94038ee1cb20413a6
MakenaMwirigi <makenasharon46@gmail.com>
2025-08-21 16:13:45 +0300

    react event handling assignment

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Keypad from "./Keypad";
+import EyesOnMe from "./EyesOnMe";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Events Lab</h1>
+      <Keypad />
+      <EyesOnMe />
+    </div>
+  );
+}
+
+export default App;
